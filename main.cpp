2577ab99abc052ff20667385ca239ef1227c9848
figerfly <figerfly@gmail.com>
2021-02-24 21:38:54 +0800

    Create main.cpp
    example

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <cstdio>
+#include <cstdlib>
+#include <string>
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+  printf("hello-world\n");
+  
+  cout << "hello world" << endl;
+}
